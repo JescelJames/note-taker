922231f8c699125a969c8d0ee0143a8179f8e5e1
Jescel James <jesceljames@gmail.com>
2023-12-30 06:14:27 -0500

    made changes for ease of reading

--- server.js
+++ server.js
@@ -1,51 +1,58 @@
-// DEPENDENCIES
-// express
-const express = require('express');
-const path =  require('path');
-const db = require('./db/db.json');
-console.log(db);
+// DEPENDENCIES __________________________________________
+   
+    const express = require('express');
+    const path =  require('path');
+    const db = require('./db/db.json');
+    console.log(db);
 
 
-// DATA
+// DATA ___________________________________________________
+    //
 
 
-// APP / PORT
-const app = express();
-const PORT = process.env.PORT || 3000;
+// APP/PORT ________________________________________________
+
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
 
-// MIDDLEWARES
+// MIDDLEWARES ______________________________________________
 
-// static assets
-app.use(express.static('public'));
-
-
-// stuff to get the req.body
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+    // static assets
+    app.use(express.static('public'));
+    // stuff to get the req.body
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
 
+// ROUTES ____________________________________________________
 
-// ROUTES
-// app.get('/', (request, response) => response.send('This is the homepage http://localhost:3001'));
-
-// GET/ HTML Routes
-app.get('/notes', (request, response) => 
-    response.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+    // GET - index homepage --------------------------------
+        app.get('/', (req, res) => res.send('This is the homepage http://localhost:3001'));
 
 
-// GET/ API Routes
-app.get('/api/notes', (request, response) => response.json(db));
+    // GET - notes.html -------------------------------------
+        app.get('/notes', (req, res) => 
+            response.sendFile(path.join(__dirname, 'public/notes.html'))
+        );
 
 
+    // GET - api notes  -------------------------------------
+        app.get('/api/notes', (req, res) => res.json(db));
 
 
+    // POST -     
+        app.post('/api/notes', (req, res) => {
+            // Handle saving the new note
+        });
 
-//404 route
-app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'public/404.html'))
-});
 
-// START SERVER
-app.listen(PORT, () => console.log(`Server live at http://localhost:${PORT}` ));
+    // GET - fallback route ---------------------------------
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'public/404.html'))
+        });
+
+
+// START SERVER ______________________________________________
+
+    app.listen(PORT, () => console.log(`Server live at http://localhost:${PORT}` ));
